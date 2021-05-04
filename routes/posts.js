dd837850beb6c821927a0492c58ec6f7cfaa2850
Bob Vawter <bobvawter@msn.com>
2021-05-04 17:53:23 -0400

    image upload working

--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-//const multer = require("multer");
-//const upload = multer({ dest: "public/uploads/" });
-//const { storage } = require("../middleware/multer");
+const multer = require("multer");
+const upload = multer({ dest: "public/uploads/" });
+const { storage } = require("../middleware/multer");
 const postsController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes - simplified for now
 router.get("/:id", ensureAuth, postsController.getPost);
 
-router.post("/createPost", postsController.createPost);
+router.post("/createPost", upload.single("file"), postsController.createPost);
 
 router.put("/likePost/:id", postsController.likePost);
 
