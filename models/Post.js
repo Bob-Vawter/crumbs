b586aec0eee74ad4aec8ebc1c0ed9c6079ea0316
Bob Vawter <bobvawter@msn.com>
2021-05-04 13:09:27 -0400

    first commit

---
+++ models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  crumb: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  userName: {
+    type: String,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
