bdb8336696c2c0b2a31863a343fb9aaa382c90cc
Bob Vawter <bobvawter@msn.com>
2021-05-10 16:33:30 -0400

    error messages when trying to change username email

--- controllers/settings.js
+++ controllers/settings.js
@@ -11,28 +11,52 @@ module.exports = {
   },
   changeUserName: async (req,res)=>{
     try{
-      await User.findOneAndUpdate({_id: req.user.id},
-        {$set: {userName : req.body.newUserName}})
-        res.redirect('back')
-      }catch(err){
-        console.log(err)
-      }
-    },
-    changeUserEmail: async (req,res)=>{
-      try{
-        const validationErrors = [];
-        if (!validator.isEmail(req.body.newUserEmail))
-          validationErrors.push({ msg: "Please enter a valid email address." });
-        if (validationErrors.length) {
-          req.flash("errors", validationErrors);
-          return res.redirect("back");
-        }
-
+      User.findOne({ userName: req.body.newUserName },
+        (err, existingUser) => {
+          if (err) {
+            return next(err);
+          }
+          if (existingUser) {
+            req.flash("errors", {
+              msg: "Account with that username already exists.",
+            });
+            return res.redirect("back");
+          }
+        })
         await User.findOneAndUpdate({_id: req.user.id},
-          {$set: {email : req.body.newUserEmail}})
+          {$set: {userName : req.body.newUserName}})
           res.redirect('back')
         }catch(err){
           console.log(err)
         }
       },
-    }
+      changeUserEmail: async (req,res)=>{
+        try{
+          const validationErrors = [];
+          if (!validator.isEmail(req.body.newUserEmail))
+          validationErrors.push({ msg: "Please enter a valid email address." });
+          if (validationErrors.length) {
+            req.flash("errors", validationErrors);
+            return res.redirect("back");
+          }
+          User.findOne({ email: req.body.newUserEmail },
+            (err, existingUser) => {
+              if (err) {
+                return next(err);
+              }
+              if (existingUser) {
+                req.flash("errors", {
+                  msg: "Account with that Email already exists.",
+                });
+                return res.redirect("back");
+              }
+            })
+
+          await User.findOneAndUpdate({_id: req.user.id},
+            {$set: {email : req.body.newUserEmail}})
+            res.redirect('back')
+          }catch(err){
+            console.log(err)
+          }
+        },
+      }
